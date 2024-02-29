6b1a7f04ab5aa03916c175c572bac5a3ce2af36b
Alba Giraldo <agiraldomolina@gmail.com>
2024-02-28 23:40:01 -0500

    get Top products router

--- api/routes/product.route.js
+++ api/routes/product.route.js
@@ -1,10 +1,11 @@
 import express from 'express';
 import { verifyToken } from '../middlewares/verifyUser.js';
-import { createProduct, deleteProduct, getProductById, getProducts, updateProduct } from '../controllers/product.controller.js';
+import { createProduct, deleteProduct, getProductById, getProducts, updateProduct, getTopProducts } from '../controllers/product.controller.js';
 
 const router = express.Router();
 
 router.post('/create', verifyToken, createProduct);
+router.get('/top', getTopProducts);
 router.delete('/delete/:productId', verifyToken, deleteProduct);
 router.put('/update/:productId', verifyToken, updateProduct);
 router.get('/',getProducts),
